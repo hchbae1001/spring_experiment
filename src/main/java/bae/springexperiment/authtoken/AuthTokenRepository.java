9bf4d9b3f9253360c8a83dead4d2821dba2110f3
hchbae1001 <hchbae1001@gmail.com>
2024-07-24 16:03:32 +0900

    Feat: AuthTokenRepository

--- src/main/java/bae/springexperiment/authtoken/AuthTokenRepository.java
+++ src/main/java/bae/springexperiment/authtoken/AuthTokenRepository.java
@@ -1,9 +1,20 @@
 package bae.springexperiment.authtoken;
 
 import bae.springexperiment.entity.AuthToken;
+import bae.springexperiment.entity.enumerate.DeviceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AuthTokenRepository extends JpaRepository<AuthToken, Long> {
+    List<AuthToken> findAllByMemberId(long member_id);
+    Optional<AuthToken> findAuthTokenByMemberIdAndDeviceType(Long member_id, DeviceType deviceType);
+    Optional<AuthToken> findByRefreshToken(String refreshToken);
+
+    void deleteAllByMemberId(Long member_id);
+    void deleteByMemberIdAndRefreshToken(Long member_id, String refreshToken);
+    void deleteByMemberIdAndDeviceType(Long member_id, DeviceType deviceType);
 }
